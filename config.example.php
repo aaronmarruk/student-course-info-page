3b85bfa6772154804d2106af8881fbc14f712052
Tom Readings <digitisatom@gmail.com>
2014-09-08 11:27:47 +0100

    example config file

--- config.example.php
+++ config.example.php
@@ -6,16 +6,17 @@ include 'moodlequery/config.php';
 
 // DB Config
 Config::write('db.host', 'localhost');
+Config::write('db.type', 'mysql');
 Config::write('db.port', '');
-Config::write('db.basename', 'learningspace');
-Config::write('db.user', 'root');
-Config::write('db.password', 'root');
+Config::write('db.basename', 'moodle_26');
+Config::write('db.user', 'moodle_26');
+Config::write('db.password', 'm768zVWyH3c5Hyez');
 
 Config::write('moodle.config', $CFG);
 
-Config::write('site.root', 'http://localhost:8888/learningspace');
-Config::write('site.root.scip', 'http://localhost:8888/learningspace/rdrctr');
-Config::write('site.logout', 'http://localhost:8888/learningspace/logout');
+Config::write('site.root', 'http://localhost/moodle_26');
+Config::write('site.root.scip', 'http://localhost/moodle_26/scip');
+Config::write('site.logout', 'http://localhost/moodle_26/logout');
 
 // Project Config
-Config::write('path', 'http://localhost:8888/rdrctr');
+Config::write('path', 'http://localhost/moodle_26/scip');
