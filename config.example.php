90ffd135973bf6c04015feb0d212421eaf10bc81
Aaron Marr <aaronmarruk@gmail.com>
2014-09-19 14:30:06 +0100

    Modify config to reflect new site root structure

--- config.example.php
+++ config.example.php
@@ -2,21 +2,11 @@
 
 use lib\Config;
 
-include 'moodlequery/config.php'; 
-
-// DB Config
-// Config::write('db.host', 'localhost');
-// Config::write('db.type', 'mysql');
-// Config::write('db.port', '');
-// Config::write('db.basename', 'moodle_26');
-// Config::write('db.user', 'moodle_26');
-// Config::write('db.password', 'm768zVWyH3c5Hyez');
+include 'moodlequery/config.php';
 
 Config::write('moodle.config', $CFG);
-
 Config::write('site.root', 'http://localhost:8888/learningspace');
-Config::write('site.root.scip', 'http://localhost:8888/learningspace/rdrctr');
+Config::write('site.root.scip', 'http://localhost:8888/learningspace/apps/scip');
 Config::write('site.logout', 'http://localhost:8888/learningspace/logout');
-
 // Project Config
-Config::write('path', 'http://localhost/learningspace/rdrctr');
+Config::write('path', 'http://localhost/learningspace/apps/scip');
